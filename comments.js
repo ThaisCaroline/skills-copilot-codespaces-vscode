95871d3fb114b1f80e85736256a4050d1e5ad08b
Thais Carolyne S.S. <thaiscaroline450@gmail.com>
2025-03-25 13:14:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+  }
+  ).listen(8080);
+    console.log('Server running on port 8080.');
+    
